b00768d978bfe05a704b6e29cdd97d50b3509fe1
Abner Duarte <duarte.abner.ad@gmail.com>
2020-08-18 22:51:13 -0300

    17-08-2020

---
+++ Engenharia-de-software/Aula02.py
@@ -0,0 +1,26 @@
+#Crises dos Softwares 
+    #Mitos sobre os software
+    #Causam confusão e desinformação: Nascem do ambito administrativo, dos proprioos funcionarios da area e dos clientes;
+    #Mitos administrativos: 
+    # Ex: 
+    # A. existencia de manual de padroes e procedimentos para construção de um software; 
+    # B. promessas de tecnologias novas e revolucionarias, isso não garante uma boa construção de software; 
+    # C. Projetos atrasados, não se resolvem com a aplicação de mais desenvolvedores para romper o atraso. Essa relação involve CURVA DE CONHECIMENTO, isso quer dizer que nem sempre a quantidade irá se sobrepor sobre a qualidade; 
+#CURVA DE CONHECIMENTO: Leva em consideração o tempo de ambientação; 
+    #Escopo: Esse elemento considera a ideia e objetivos de um cliente; Não é simplesmente uma montagem de qualquer escopo que tornará o trabalho eficiente e pontual; O escopo deve obedecer alguns padrões para fácil compreensão daquele que o tornará um programa; Consequencias negativas: projetos com prazos atrasados e afins; 
+    #Mito de que as mudanças são facilmente adaptáveis as flexibilidades dos software toda mudança deve ser pensada e estruturada. Lei de Mayer: Lei do 10 x 1, isso significa dizer que a cada fase do desenvolvimento o custo para mudança aumenta na proporção de 10x. Ex: Se na fase inicial o custo de mudança era R$ 10,00, na próxima fase será de R$ 100,00 e assim por diante. 
+#Mitos dos profissionais de software: 
+# A. O trabalho de software nunca estará completo a partir do momento que o software está circulando em ambiente de produção que o trabalho está feito, é necessário dar o suporte em tarefas de manutenção, documentação e afins, dando destaque a fase de operação do software, fazendo com que o cliente tenha noção das novas demandas; 
+#Mito da qualidade: Enquanto não tiver o programa funcionando não há maneira de avaliar sua qualidade, é possível avaliar a qualidade desde as fases iniciais, compõe dois grandes grupos: 
+# A. Processo de Software que desembca no produto de software, o projeto de software vai denunciar como o produto vai ser entregue; 
+# B.Produto; 
+#Preocupações: É sistematizar o processo de criação e manutenção de software. 
+#Definição da engenharia: 1) É uma abordagem sistematica, para desenvolvimento, manutenção  e operação; 
+    #A preocupação é entrega dos projetos dentro dos prazos, qualidades e estimativas de gastos estabelecidos; 
+    #Foco na gerencialidade da engenharia; 
+#Metas da E.S: Melhorar a qualidade de entrega dos softwares aumentando a produtividade do tecnico e satisfação dos clientes; 
+#Elementos da E.S: 
+    #Metodos: detalhes de como construir o Software: atividades para que se possa chegar ao fim com um software com qualidade;
+    #Ferramentas: Suporte ao metodo; ex: cronograma;
+    #Procedimentos: A maneira de usar a ferramenta para antender a atividade do metodo; 
+    #Atividade: conjunto de metodos, ferramentas e procedimentos vão gerar as atividades principais dentro dos paradigmas da engenharia;  
